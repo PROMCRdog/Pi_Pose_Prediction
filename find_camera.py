922214ca757e0ee1849999e43fb275aca1fc9545
PROMCRdog <michaellwwang@gmail.com>
2024-03-23 20:57:54 +0800

    find_camera code updated

--- find_camera.py
+++ find_camera.py
@@ -1,1 +1,16 @@
-#hi
+import cv2
+
+for camera_id in range(0, 20):
+    cap = cv2.VideoCapture(camera_id)
+    if cap is None or not cap.isOpened():
+        print(f"No camera found at index {camera_id}")
+    else:
+        ret, frame = cap.read()
+        if ret:
+            print(f"camera found at index {camera_id}")
+            cv2.imshow('frame', frame)
+            cv2.waitKey(0)
+            cv2.destroyWindow('frame')
+        cap.release()
+
+cv2.destroyWindow
